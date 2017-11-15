e96d6ec46505de3727aba9a8d9fa8da8e0d35fac
AlexFin <alexey.finohenov@gmail.com>
2017-11-15 20:59:03 +0200

    my little css generator :)

--- script.js
+++ script.js
@@ -4,10 +4,30 @@
 // 	var sel = document.getElementById('mySelect').selectedIndex;
 // 	var options = document.getElementById('mySelect').options;
 // 	alert('Choosed option ' +options[sel].text );
-// }
 
-function fun1() {
-	var rng = document.getElementById('r1');
-	var div=document.getElementById('test');
-	div.style.width=(rng.value * 3)+'px';
+function fun1 (){
+	var div = document.getElementById('block1');
+	
+	var rtl=document.getElementById('rtl').value; //top left	
+	var rtr=document.getElementById('rtr').value; //top right
+	var rbr=document.getElementById('rbr').value; //bottom rigth
+	var rbl=document.getElementById('rbl').value; //bottom left
+
+	var ttl=document.getElementById('ttl');
+	var ttr=document.getElementById('ttr');
+	var tbr=document.getElementById('tbr');
+	var tbl=document.getElementById('tbl');
+
+
+
+	ttl.value = (rtl + ' px');
+	ttr.value = (rtr + ' px');
+	tbr.value = (rbr + ' px');
+	tbl.value = (rbl + ' px');
+
+	var div = document.getElementById('block1');
+	div.style.borderRadius = (rtl+'px ' + rtr +'px ' + rbr +'px ' + rbl +'px ');
+
+
+
 }
